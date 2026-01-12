d9d3f9521553547389a4d9bf848ebcb8107bc328
davidaroussel <david.roussel@hotmail.com>
2026-01-12 17:59:31 -0500

    going to work at uni

--- WiFiHeadstage V2/FPGA/FPGA_Intan_Driver/impl_1/FIFO_MEM.v
+++ WiFiHeadstage V2/FPGA/FPGA_Intan_Driver/impl_1/FIFO_MEM.v
@@ -1,5 +1,5 @@
 // Verilog netlist produced by program LSE 
-// Netlist written on Thu Jan  8 17:34:31 2026
+// Netlist written on Fri Jan  9 16:35:23 2026
 // Source file index table: 
 // Object locations will have the form @<file_index>(<first_ line>[<left_column>],<last_line>[<right_column>])
 // file 0 "c:/lscc/radiant/2023.2/ip/avant/fifo/rtl/lscc_fifo.v"
